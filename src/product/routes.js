f4bf1909dab38a68f8a457cc94a508ea941dcb6e
Lucas Bertin <lucas.bertin@lambda3.com.br>
2018-03-09 12:43:12 -0300

    Changed to use proto contract

--- src/product/routes.js
+++ src/product/routes.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import { sendMessage } from 'queue'
+import { Product } from 'messages'
 
 /* disabling next line to enable jsdoc type inference */
 //eslint-disable-next-line no-unused-vars
@@ -16,12 +17,8 @@ import Koa from 'koa'
 */
 export async function findProducts(ctx) {
   const { channel, queueName } = ctx.amqp
-  ctx.body = await sendMessage(JSON.stringify({ 
-    name: 'Product x',
-    description: 'Description x',
-    sku: 'asd123a',
-    created_at: new Date() 
-  }), queueName, channel)
+  const product = await new Product('Product x', 'Description x', 'asd123a').encode()
+  ctx.body = await sendMessage(product, queueName, channel)
 }
 
 export default new Router({ prefix: '/product:s?' })
